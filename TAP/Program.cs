98f99c098b89e54e537f8d2bef16dd2236bf61d5
kulivers <kulivers@mail.ru>
2021-10-13 23:44:06 +0300

    Added await examples

--- TAP/Program.cs
+++ TAP/Program.cs
@@ -1,17 +1,51 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace ThreadPoolExample
+namespace TAP
 {
     class Program
     {
+        static void WatchThreadsWhileAwaiting()
+        {
+            AwaitPlayground.WatchThreadsWhileAwaiting();
+        }
+
         static void DiffWithAwaitAndWithout()
         {
-            AwaitDifference.AsyncParallelDiff();
+            AwaitPlayground.AsyncParallelDiff();
             Console.ReadLine();
+        }
+
+
+        static async void CallingTasks()
+        {
+            static async Task<int> SomeAsync()
+            {
+                for (int i = 1; i <= 99; i++)
+                {
+                    Console.Write(i + " ");
+                    Thread.Sleep(1);
+                }
+
+                Console.WriteLine();
+
+                return 1;
+            }
+
+            Task<int> task = SomeAsync();
+            var a = await task;
+            Console.WriteLine(a);
+            //something with await
         }
 
         static void Main()
         {
+            
+
+
+            // WatchThreadsWhileAwaiting();
+            Console.ReadLine();
         }
     }
 }
