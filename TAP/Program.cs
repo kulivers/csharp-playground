65c00201f247e9297c2b0f8238462689fbee3a29
kulivers <kulivers@mail.ru>
2021-10-29 14:38:13 +0300

    no text, trying to commit

--- TAP/Program.cs
+++ TAP/Program.cs
@@ -1,47 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
+using TAP.Exercises;
 
 namespace TAP
 {
-    class MultiThreadList
-
-    {
-        private List<int> Ls { get; set; }
-
-        public async Task FillEmptyList(int elementsCount, int threadCount)
-        {
-            var elsInOneArr = elementsCount / threadCount - 1;
-            var elsInLastThread = elementsCount - elsInOneArr * threadCount;
-            var locker = new object();
-            Parallel.For(0, threadCount, i =>
-            {
-                var localList = new List<int>();
-                // var localList = new ThreadLocal<List<int>>();
-                var rand = new ThreadLocal<Random>(() => new Random()).Value;
-                // localList.Value.Add(rand.Next(0, 20));
-                localList.Add(rand.Next(0, 20));
-                lock (locker)
-                {
-                    Ls.AddRange(localList);
-                }
-            });
-        }
-
-        public void Show()
-        {
-            Ls.ForEach(Console.WriteLine);
-        }
-    }
-
     class Program
     {
         static void Main()
         {
-            
+         
+        }
 
+        static void ShowTrackTimesOfFillers()
+        {
+            var timeMultiThreadArray = TimeTracker(() =>
+            {
+                var multiThreadList = new MultiThreadList(20001);
+                multiThreadList.FillArrayRandoms(20);
+            });
+            var timeConcurrent = TimeTracker(() =>
+            {
+                var multiThreadList = new MySelfFillingConcurrentDictionary();
+                multiThreadList.FillTheDictRandNumbers(20);
+            });
+            var timeDictionary = TimeTracker(() =>
+            {
+                var multiThreadList = new MySelfFillingDictionary();
+                multiThreadList.FillTheDictRandNumbers(20);
+            });
+
+            var timeSynchronislyLocking = TimeTracker(() =>
+            {
+                var multiThreadList = new MySelfFillingDictionaryWithoutLocking();
+                multiThreadList.FillTheDictRandNumbers(20);
+            });
+            Console.WriteLine(timeConcurrent);
+            Console.WriteLine(timeDictionary);
+            Console.WriteLine(timeSynchronislyLocking);
+            Console.WriteLine(timeMultiThreadArray);
         }
 
         protected static long TimeTracker(Action action)
@@ -51,7 +47,7 @@ namespace TAP
             action();
             stopwatch.Stop();
             var span = stopwatch.Elapsed;
-            return span.Milliseconds;
+            return span.Ticks;
         }
     }
 }
